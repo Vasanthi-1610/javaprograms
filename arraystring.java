03a58810619dd2d87a96e8e25e0963c294bcf41f
Vasanthi-1610 <149652734+Vasanthi-1610@users.noreply.github.com>
2024-05-10 11:00:21 +0530

    Add files via upload

---
+++ arraystring.java
@@ -0,0 +1,21 @@
+package Arraysbasics;
+import java.util.Scanner;
+
+public class arraystring {
+
+	public static void main(String[] args) {
+		int x;
+		String[] a=new String[5];
+		Scanner sc=new Scanner(System.in);
+		System.out.println("enter the stings:");
+		for(x=0;x<a.length;x++)
+		{
+			//a[x]=sc.nextInt();
+		}
+		//for(y=0;y<)
+		
+		
+
+	}
+
+}
